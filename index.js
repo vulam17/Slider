b79cdd5f5dce05a1c8ad2f35e12c6eb82eb6c690
vulam.17 <vulam.17@gmail.com>
2022-09-29 21:53:18 +0700

    lan 2

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 let img = [document.getElementById("img1"),
 document.getElementById("img2"),
 document.getElementById("img3"),
-]
-let imgName = document.getElementById("imgName")
-let pos = 1
-let interval = setInterval(right,5000)
+] // Tạo mảng các phần tử ảnh
+let imgName = document.getElementById("imgName") // Ghi chú đang hiện ảnh số bao nhiêu
+let pos = 1 // Vị trí ảnh hiện tại
+let interval = setInterval(right,5000) // Cài đặt tự động gọi hàm right (trôi xoay tua sang phải) sau mỗi 5 giây
 function left()
 {
     pos--;
-    if (pos === 0) pos = 3
-    display(pos)
+    if (pos === 0) pos = 3 // Vị trí ảnh từ 1 đến 3. Nếu pos = 0 thì gán pos = 3
+    display(pos) 
 }
 function right()
 {
     pos++;
-    if (pos === 4) pos = 1
+    if (pos === 4) pos = 1 // Vị trí ảnh từ 1 đến 3. Nếu pos = 4 thì gán pos = 1
     display(pos)
 }
-function display(pos)
+function display(pos) // Hiển thị ảnh
 {
     for(i = 0; i < 3; i++)
         {
             if(i === (pos-1))
             {
-                img[i].style.display = "block"
-                imgName.innerHTML = "Ảnh số: " + pos
+                img[i].style.display = "block" // Hiện 1 ảnh 
+                imgName.innerHTML = "Ảnh số: " + pos // Ghi chú 
             }
             else
             {
-                img[i].style.display = "none"
+                img[i].style.display = "none" // Các ảnh còn lại không hiện
             }
         }
 }
